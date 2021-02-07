99220f682c7c40d1d0bbbb6bc8211ae5926b9296
i3lawy <78707218+i3lawy@users.noreply.github.com>
2021-02-08 02:44:50 +0300

    Update AutoSelect.js

--- AutoSelect.js
+++ AutoSelect.js
@@ -5,15 +5,8 @@
 // @description This script to auto select from ListBox.
 // @include     https://*.almubasher.com.sa/NewECorporate/p/billPaymentService/addNewBillAction.menu*
 // @include     https://*.almubasher.com.sa/NewECorporate/p/billPaymentService/addNewBillAction.do*
-// @downloadURL https://github.com/i3lawy/dhlscripts/raw/main/AutoSelect.js
-// @updateURL   https://github.com/i3lawy/dhlscripts/raw/main/AutoSelect.js
+// @downloadURL https://github.com/i3lawy/DHLPRSCRIPT/raw/main/AutoSelect.js
+// @updateURL   https://github.com/i3lawy/DHLPRSCRIPT/raw/main/AutoSelect.js
 // @grant none
 // ==/UserScript==
-for(i = 55; i < 70; i++)
-{
-   if(document.getElementById('indexBillCode').options[i].text == "Customs")
-   {
-     document.getElementById('indexBillCode').value = i;
-     console.log("The Customes Selected with ID: " + i);
-   }
-}
+var _0x5e76=['text','51ustSpw','2777qDFvVj','1gQbvDS','51lFxaPJ','1XaiwhB','7nqSgFb','5452BjLIrl','value','The\x20Customes\x20Selected\x20with\x20ID:\x20','getElementById','Customs','52181vmUsTP','log','729154ktCfMU','options','87845viTryk','5723yLIiMB','351881pkXTrZ'];var _0x440f=function(_0x187ce5,_0x323bab){_0x187ce5=_0x187ce5-0x1c4;var _0x5e7630=_0x5e76[_0x187ce5];return _0x5e7630;};var _0x246f7f=_0x440f;(function(_0x40ea3a,_0x3b03be){var _0x1ccb7c=_0x440f;while(!![]){try{var _0xd703cf=-parseInt(_0x1ccb7c(0x1c6))*-parseInt(_0x1ccb7c(0x1ca))+parseInt(_0x1ccb7c(0x1d5))*parseInt(_0x1ccb7c(0x1c5))+-parseInt(_0x1ccb7c(0x1c9))*-parseInt(_0x1ccb7c(0x1d0))+parseInt(_0x1ccb7c(0x1c8))*parseInt(_0x1ccb7c(0x1cb))+-parseInt(_0x1ccb7c(0x1d4))+parseInt(_0x1ccb7c(0x1c7))*parseInt(_0x1ccb7c(0x1d6))+-parseInt(_0x1ccb7c(0x1d2));if(_0xd703cf===_0x3b03be)break;else _0x40ea3a['push'](_0x40ea3a['shift']());}catch(_0x5ad2f2){_0x40ea3a['push'](_0x40ea3a['shift']());}}}(_0x5e76,0x2b12b));for(i=0x37;i<0x46;i++){document['getElementById']('indexBillCode')[_0x246f7f(0x1d3)][i][_0x246f7f(0x1c4)]==_0x246f7f(0x1cf)&&(document[_0x246f7f(0x1ce)]('indexBillCode')[_0x246f7f(0x1cc)]=i,console[_0x246f7f(0x1d1)](_0x246f7f(0x1cd)+i));}
