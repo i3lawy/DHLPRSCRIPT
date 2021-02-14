592acfc97d2d240736da4da27fbcc3c30c50a1b8
i3lawy <78707218+i3lawy@users.noreply.github.com>
2021-02-14 03:02:46 +0300

    Update AutoBack.js

--- AutoBack.js
+++ AutoBack.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name AutoBack
 // @namespace Ali AlMubarak Scripts
-// @version 1.0
+// @version 1.2
 // @description This script to back automatically from end of the bill.
 // @include     https://*.almubasher.com.sa/NewECorporate/p/billPaymentService/billDetailsAcceptAction.do*
 // @downloadURL https://github.com/i3lawy/DHLPRSCRIPT/raw/main/AutoBack.js
