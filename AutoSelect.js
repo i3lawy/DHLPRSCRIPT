971042ecdcf32d6b8155e219da3fbbde1d151768
i3lawy <78707218+i3lawy@users.noreply.github.com>
2021-02-21 03:08:31 +0300

    Update AutoSelect.js

--- AutoSelect.js
+++ AutoSelect.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name AutoSelectCustomes
 // @namespace Ali AlMubarak Scripts
-// @version 1.2
+// @version 1.3
 // @description This script to auto select from ListBox.
 // @include     https://*.almubasher.com.sa/NewECorporate/p/billPaymentService/addNewBillAction.menu*
 // @include     https://*.almubasher.com.sa/NewECorporate/p/billPaymentService/addNewBillAction.do*
