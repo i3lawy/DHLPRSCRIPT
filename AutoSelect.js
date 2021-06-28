8f875ecf45516d7ef46a5838d54a9e3d01530283
i3lawy <78707218+i3lawy@users.noreply.github.com>
2021-06-28 11:08:00 +0300

    Update AutoSelect.js

--- AutoSelect.js
+++ AutoSelect.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name AutoSelectCustomes
-// @namespace Ali AlMubarak Scripts
-// @version 1.4
+// @namespace Ali AlMubarak Scripts | TEL +966 58 160 4111
+// @version 1.5
 // @description This script to auto select from ListBox.
 // @include     https://*.almubasher.com.sa/NewECorporate/p/billPaymentService/addNewBillAction.menu*
 // @include     https://*.almubasher.com.sa/NewECorporate/p/billPaymentService/addNewBillAction.do*
@@ -9,4 +9,4 @@
 // @updateURL   https://github.com/i3lawy/DHLPRSCRIPT/raw/main/AutoSelect.js
 // @grant none
 // ==/UserScript==
-var _0x5e76=['text','51ustSpw','2777qDFvVj','1gQbvDS','51lFxaPJ','1XaiwhB','7nqSgFb','5452BjLIrl','value','The\x20Customes\x20Selected\x20with\x20ID:\x20','getElementById','Customs','52181vmUsTP','log','729154ktCfMU','options','87845viTryk','5723yLIiMB','351881pkXTrZ'];var _0x440f=function(_0x187ce5,_0x323bab){_0x187ce5=_0x187ce5-0x1c4;var _0x5e7630=_0x5e76[_0x187ce5];return _0x5e7630;};var _0x246f7f=_0x440f;(function(_0x40ea3a,_0x3b03be){var _0x1ccb7c=_0x440f;while(!![]){try{var _0xd703cf=-parseInt(_0x1ccb7c(0x1c6))*-parseInt(_0x1ccb7c(0x1ca))+parseInt(_0x1ccb7c(0x1d5))*parseInt(_0x1ccb7c(0x1c5))+-parseInt(_0x1ccb7c(0x1c9))*-parseInt(_0x1ccb7c(0x1d0))+parseInt(_0x1ccb7c(0x1c8))*parseInt(_0x1ccb7c(0x1cb))+-parseInt(_0x1ccb7c(0x1d4))+parseInt(_0x1ccb7c(0x1c7))*parseInt(_0x1ccb7c(0x1d6))+-parseInt(_0x1ccb7c(0x1d2));if(_0xd703cf===_0x3b03be)break;else _0x40ea3a['push'](_0x40ea3a['shift']());}catch(_0x5ad2f2){_0x40ea3a['push'](_0x40ea3a['shift']());}}}(_0x5e76,0x2b12b));for(i=0x37;i<0x46;i++){document['getElementById']('indexBillCode')[_0x246f7f(0x1d3)][i][_0x246f7f(0x1c4)]==_0x246f7f(0x1cf)&&(document[_0x246f7f(0x1ce)]('indexBillCode')[_0x246f7f(0x1cc)]=i,console[_0x246f7f(0x1d1)](_0x246f7f(0x1cd)+i));}
+var _0x24e1=['1897nXnrIW','911yjGbnJ','Customs','getElementById','indexBillCode','471oSWWuq','7avkBEX','55VMeiRu','options','522920qxsCpr','1012304cWomUB','839PUNfuu','84719bEGxhn','1243lqUzjc','value','1045438HotqHo'];var _0x286b13=_0x25cc;function _0x25cc(_0x2ed9ab,_0x205e12){return _0x25cc=function(_0x24e153,_0x25cc63){_0x24e153=_0x24e153-0x192;var _0x1627f5=_0x24e1[_0x24e153];return _0x1627f5;},_0x25cc(_0x2ed9ab,_0x205e12);}(function(_0x3f2249,_0x415369){var _0x4df922=_0x25cc;while(!![]){try{var _0x58d4a5=parseInt(_0x4df922(0x19e))+-parseInt(_0x4df922(0x19a))*parseInt(_0x4df922(0x196))+parseInt(_0x4df922(0x19f))*parseInt(_0x4df922(0x194))+-parseInt(_0x4df922(0x1a0))*parseInt(_0x4df922(0x19c))+-parseInt(_0x4df922(0x198))+parseInt(_0x4df922(0x199))+-parseInt(_0x4df922(0x195))*parseInt(_0x4df922(0x19b));if(_0x58d4a5===_0x415369)break;else _0x3f2249['push'](_0x3f2249['shift']());}catch(_0x18005f){_0x3f2249['push'](_0x3f2249['shift']());}}}(_0x24e1,0xa0576));for(i=0x46;i<0x49;i++){document[_0x286b13(0x192)]('indexBillCode')[_0x286b13(0x197)][i]['text']==_0x286b13(0x1a1)&&(document[_0x286b13(0x192)](_0x286b13(0x193))[_0x286b13(0x19d)]=i);}
