af38c0d91776ebd03e7a631692978dbfaf9a74e5
i3lawy <78707218+i3lawy@users.noreply.github.com>
2021-09-14 18:10:12 +0300

    Update DCEAutoFill.js

--- DCEAutoFill.js
+++ DCEAutoFill.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name DCE AutoFill
 // @namespace Ali AlMubarak @ +966581604111 | contact.alawy@gmail.com
-// @version 1.7.3
+// @version 1.7.4
 // @description This script to auto select items, remove not required to fill it.
 // @include https://dcesa2.dhl.com/*
 // @downloadURL https://github.com/i3lawy/DHLPRSCRIPT/raw/main/DCEAutoFill.js
