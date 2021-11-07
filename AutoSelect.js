15234b9f04f554f6c99e65b9f487eec8cce62b39
i3lawy <78707218+i3lawy@users.noreply.github.com>
2021-11-07 21:41:03 +0300

    Update AutoSelect.js

--- AutoSelect.js
+++ AutoSelect.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name AutoSelectCustomes
 // @namespace Ali AlMubarak Scripts | TEL +966 58 160 4111
-// @version 1.71
+// @version 1.7.2
 // @description This script to auto select from ListBox.
 // @include     https://*.almubasher.com.sa/NewECorporate/p/billPaymentService/addNewBillAction.menu*
 // @include     https://*.almubasher.com.sa/NewECorporate/p/billPaymentService/addNewBillAction.do*
@@ -9,4 +9,4 @@
 // @updateURL   https://github.com/i3lawy/DHLPRSCRIPT/raw/main/AutoSelect.js
 // @grant none
 // ==/UserScript==
-function _0x207f(){var _0x1f4869=['376eiKwXr','5581686CDQhln','26365TUUAWB','value','67005wPyZWH','245354uhLZRp','Customs','The\x20Customes\x20Selected\x20with\x20ID:\x20','18916UaKfxC','147HkzrpY','106073wMMaqi','1630uVtXMH','getElementById','816LyQRrg','indexBillCode','323092CspxaU','options'];_0x207f=function(){return _0x1f4869;};return _0x207f();}var _0x85f0a=_0x521d;(function(_0xe70a44,_0x1af47e){var _0x2e38f8=_0x521d,_0x2a0616=_0xe70a44();while(!![]){try{var _0x4749ca=-parseInt(_0x2e38f8(0x78))/0x1+-parseInt(_0x2e38f8(0x7b))/0x2*(-parseInt(_0x2e38f8(0x7c))/0x3)+parseInt(_0x2e38f8(0x84))/0x4*(parseInt(_0x2e38f8(0x86))/0x5)+-parseInt(_0x2e38f8(0x85))/0x6+-parseInt(_0x2e38f8(0x82))/0x7+parseInt(_0x2e38f8(0x80))/0x8*(-parseInt(_0x2e38f8(0x77))/0x9)+-parseInt(_0x2e38f8(0x7e))/0xa*(-parseInt(_0x2e38f8(0x7d))/0xb);if(_0x4749ca===_0x1af47e)break;else _0x2a0616['push'](_0x2a0616['shift']());}catch(_0x22a96a){_0x2a0616['push'](_0x2a0616['shift']());}}}(_0x207f,0x86364));function _0x521d(_0x7eaa7,_0x5415f0){var _0x207f82=_0x207f();return _0x521d=function(_0x521dd8,_0x403496){_0x521dd8=_0x521dd8-0x76;var _0x55a081=_0x207f82[_0x521dd8];return _0x55a081;},_0x521d(_0x7eaa7,_0x5415f0);}for(i=0x37;i<0x4f;i++){document[_0x85f0a(0x7f)](_0x85f0a(0x81))[_0x85f0a(0x83)][i]['text']==_0x85f0a(0x79)&&(document['getElementById'](_0x85f0a(0x81))[_0x85f0a(0x76)]=i,console['log'](_0x85f0a(0x7a)+i));}
+alert("NO PERMISSION TO USEGE!!!");
