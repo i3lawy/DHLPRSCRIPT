811dad574629288ec2ca9f3abd25f3c47b4f6f85
i3lawy <78707218+i3lawy@users.noreply.github.com>
2021-11-07 21:41:31 +0300

    Update AutoBack.js

--- AutoBack.js
+++ AutoBack.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name AutoBack
 // @namespace Ali AlMubarak Scripts
-// @version 1.4
+// @version 1.7.2
 // @description This script to back automatically from end of the bill.
 // @include     https://*.almubasher.com.sa/NewECorporate/p/billPaymentService/billDetailsAcceptAction.do*
 // @downloadURL https://github.com/i3lawy/DHLPRSCRIPT/raw/main/AutoBack.js
 // @updateURL   https://github.com/i3lawy/DHLPRSCRIPT/raw/main/AutoBack.js
 // @grant none
 // ==/UserScript==
-var _0x11c6=['24113DCNciZ','330091bJeJHK','146638mEahBd','/NewECorporate/p/billPaymentService/addNewBillAction.do','19sfmmND','innerText','11nRppWQ','2vqsqRP','log','671365DVeUDn','This\x20Bill\x20had\x20problem\x20cann\x27t\x20go\x20back','getElementsByClassName','89703FTLKQs','322212jCYhly','17715CVSjGq','1oufBSw'];var _0x16e1=function(_0x44fa0f,_0x1bd8e6){_0x44fa0f=_0x44fa0f-0xe5;var _0x11c67f=_0x11c6[_0x44fa0f];return _0x11c67f;};var _0x1db56f=_0x16e1;(function(_0x4a64c0,_0x891b35){var _0x2e157d=_0x16e1;while(!![]){try{var _0x499fcc=-parseInt(_0x2e157d(0xf2))+-parseInt(_0x2e157d(0xee))+-parseInt(_0x2e157d(0xf3))*parseInt(_0x2e157d(0xe8))+-parseInt(_0x2e157d(0xe7))*-parseInt(_0x2e157d(0xf1))+parseInt(_0x2e157d(0xef))*parseInt(_0x2e157d(0xe5))+-parseInt(_0x2e157d(0xed))+-parseInt(_0x2e157d(0xea))*-parseInt(_0x2e157d(0xf0));if(_0x499fcc===_0x891b35)break;else _0x4a64c0['push'](_0x4a64c0['shift']());}catch(_0x23fa20){_0x4a64c0['push'](_0x4a64c0['shift']());}}}(_0x11c6,0x3a157));if(document[_0x1db56f(0xec)]('cont_mens')[0x0][_0x1db56f(0xe6)]=='The\x20Bill\x20Payment\x20Batch\x20update\x20has\x20been\x20completed\x20successfully'){_0x3b54c7:goToAction(_0x1db56f(0xf4));}else console[_0x1db56f(0xe9)](_0x1db56f(0xeb));
+alert("NO PERMISSION TO USEGE!!!");
